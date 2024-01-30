3e92cfaf0e4f27513000f7cb3849307d9925f629
jimish SONI <jimishsoni1990@gmail.com>
2024-01-30 21:18:39 +0530

    Get current year in copyright

--- intranet/footer.php
+++ intranet/footer.php
@@ -18,7 +18,7 @@
 	<div class="row g-0 justify-content-between align-items-center h-100">
 		<div class="col-12 col-sm-auto text-center">
 			<p class="mb-0 mt-2 mt-sm-0 text-900">
-				&copy; Copyrights 2023, All rights reserved.
+				&copy; Copyrights <?php echo date('Y'); ?>, All rights reserved.
 			</p>
 		</div>
 		<div class="col-12 col-sm-auto text-center">
